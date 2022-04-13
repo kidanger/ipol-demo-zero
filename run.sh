3aedbb969c037eb1a582172fc2d23b2ea72c617e
Anger Jérémy <angerj.dev@gmail.com>
2022-04-13 19:59:06 +0200

    ipol: add run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create compressed version
+convert -quality 99% input_0.png input_Q.jpg
+
+# Run algorithm
+/zero input_0.png input_Q.jpg
+
+# Draw results
+python3 /create_votemap.py votes.png
+python3 /create_votemap.py votes_jpeg.png
+python3 /merge_zero.py mask_f.png mask_m.png luminance.png
+
